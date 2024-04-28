1d403e90b590ed379fe8b2503d1662c7126ce401
xFleur <mels1999@live.nl>
2024-04-29 00:30:03 +0200

    update files

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import './app.scss'
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
+// import Parallax from './components/parallax/Parallax'
+import Bio from './components/bio/Bio'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Cursor from './components/cursor/Cursor'
+import Resume from './components/resume/Resume'
+import Experience from './components/experience/Experience'
+
+
+function App() {
+  return (
+    <div>
+       <Cursor />
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      {/* <section id="Services"> */}
+        {/* <Parallax type="services" /> */}
+        {/* <Parallax type="portfolio" /> */}
+      {/* </section> */}
+      <section id="Biography">
+      <Bio />
+      </section>
+      <section id="Experience">
+      <Experience />
+      </section>
+      <section id="Portfolio">
+      <Portfolio />
+      </section>
+      <section id="Resume">
+      <Resume />
+      </section>
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/bio/Bio.jsx
@@ -0,0 +1,47 @@
+import "./bio.scss";
+import { motion } from "framer-motion";
+import { useRef } from "react";
+
+const variants = {
+  initial: {
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Bio = () => {
+  const ref = useRef();
+
+  return (
+    <motion.div
+    ref={ref}
+    className="bio"
+    variants={variants}
+    initial="initial"
+    whileInView="animate"
+  >
+    <motion.div className="bio">
+      <motion.div className="wrapper">
+      <motion.h1>Biography</motion.h1>
+      <motion.p>Hej! I am Melissa! 
+        I get energy from achieving results together with others. 
+        My adaptability and creative spirit enable me to quickly navigate new systems and situations. 
+        Outside the professional field, my passion for hockey goes back to my childhood and I still actively participate in ice hockey. 
+        Besides this beloved sport, I find passion in activities such as running, mountain biking and painting. 
+        Exploring the latest technologies also fascinates me.</motion.p>
+       
+      </motion.div>
+    </motion.div>
+    </motion.div>
+  );
+};
+
+export default Bio;
