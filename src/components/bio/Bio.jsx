beb244d3be386260ce7630bdef09291dc9ccf9bb
xFleur <mels1999@live.nl>
2024-05-02 18:38:47 +0200

    bio page

--- src/components/bio/Bio.jsx
+++ src/components/bio/Bio.jsx
@@ -53,19 +53,21 @@ const Bio = () => {
     <motion.div className="bio">
       <motion.div className="wrapper">
       <motion.h1 class="header">Biography</motion.h1>
-      {/* <div class="aside aside-1"> */}
+      <div class="aside aside-1">
         <motion.img
-        class="aside aside-1"
+        // class="aside aside-1"
             variants=""
             animate="scrollButton"
             src="./images/melissa/prof/Subject5.png"
             alt=""
           />
-          {/* </div> */}
-      <motion.p class="aside aside-2">Hej! I am Melissa and I am {age} years old, enthusiastic, driven and decisive are words that describe me well. 
-      Currently, I am studying Interactive Media Technology at KTH in Stockholm. I am particularly drawn to the intersection of design and technology, which is where my skills and passions truly come together. 
-      I find the process of blending creativity with the latest technological advancements to be extremely rewarding, and I believe this is where I can make a real impact in the field.
-      </motion.p>
+          </div>
+          <div class="aside aside-2"> 
+            <motion.p >Hej! I am Melissa and I am {age} years old, enthusiastic, driven and decisive are words that describe me well. 
+            Currently, I am studying Interactive Media Technology at KTH in Stockholm. I am particularly drawn to the intersection of design and technology, which is where my skills and passions truly come together. 
+            I find the process of blending creativity with the latest technological advancements to be extremely rewarding, and I believe this is where I can make a real impact in the field.
+            </motion.p>
+          </div>
       </motion.div>
     </motion.div>
     </motion.div>
