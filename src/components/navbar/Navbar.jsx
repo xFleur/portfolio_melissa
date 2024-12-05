c5a98bc6c4b9e5af67403c66c020e5a7a3861d22
xFleur <mels1999@live.nl>
2024-12-05 01:22:12 +0100

    mini update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,30 +5,30 @@ import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <div className="navbar">
-      {/* Sidebar */}
-      <Sidebar/>
+      {/* Sidebar with round yellow circle */}
+      <div className="menu">
+        <Sidebar />
+      </div>
+
       <div className="wrapper">
-        <motion.span
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          
-          <h1>
-            <a href="#homepage">
-            MJ / Portfolio
-            </a>
-          </h1>
-          
-        </motion.span>
+        {/* Social icons on the left */}
         <div className="social">
-          <a href="https://www.linkedin.com/in/melissa-j/"  target="_blank">
+          <a href="https://www.linkedin.com/in/melissa-j/" target="_blank">
             <img src="./images/icons/linkedin.png" alt="linkedin" />
           </a>
           <a href="https://github.com/xFleur" target="_blank">
             <img src="./images/icons/github.png" alt="github" />
           </a>
         </div>
+
+        {/* Centered Portfolio text */}
+        <motion.h1
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <a href="#homepage">MJ / Portfolio</a>
+        </motion.h1>
       </div>
     </div>
   );
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -25,7 +25,7 @@ const itemVariants = {
 };
 
 const Links = () => {
-  const items = ["Homepage", "Biography", "Experience", "Portfolio", "Resume", "Contact"];
+  const items = ["Homepage", "Biography", "Portfolio", "Contact"];
 
   return (
     <motion.div className="links" variants={variants}>
