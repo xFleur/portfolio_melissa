cd9e9ec356e8aca4ef5602d9e019b0a81b5c68d2
xFleur <mels1999@live.nl>
2025-04-29 15:29:42 +0200

    dev
      "devDependencies": {
        "@types/react": "^18.2.56",
        "@types/react-dom": "^18.2.19",
        "@vitejs/plugin-react": "^4.2.1",
        "crypto-browserify": "^3.12.1",
        "eslint-plugin-react": "^7.33.2",
        "eslint-plugin-react-hooks": "^5.2.0",
        "eslint-plugin-react-refresh": "^0.4.5",
        "gh-pages": "^6.1.1",
        "vite": "^6.3.3"
      }

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,26 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio_melissa/",
+  resolve: {
+    alias: {
+      // Polyfill voor Node.js crypto module
+      crypto: 'crypto-browserify',  // Voeg de crypto polyfill toe
+    },
+  },
+  build: {
+    // Eventuele extra instellingen voor de build
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Eventuele SCSS instellingen
+      },
+    },
+  },
+
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),  // Alleen de benodigde omgevingsvariabelen doorgeven
+    'global': 'globalThis',  // Zorg ervoor dat het globale object gedefinieerd is
+  },
 })
