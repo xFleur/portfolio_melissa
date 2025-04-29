082a1c2f48f29a3a75d5028a4138b1f479e6ec3f
xFleur <mels1999@live.nl>
2025-04-29 14:35:47 +0200

    new package npm

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio_melissa/",
+  resolve: {
+    alias: {
+      // Polyfill for Node.js crypto module
+      crypto: 'crypto-browserify',
+    },
+  },
+  build: {
+    // Additional build settings if necessary
+  },
+
   css: {
     preprocessorOptions: {
       scss: {
