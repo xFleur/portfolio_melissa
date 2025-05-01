6b694ad45a10c0c6659fa1a43da417864ce59b6f
xFleur <mels1999@live.nl>
2025-05-01 14:43:48 +0200

    fix portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -36,52 +36,60 @@ const Portfolio = () => {
       <motion.h1>Portfolio</motion.h1>
 
       <div className="gallery">
+
           {/* Image 1 */}
           <motion.div className="gallery-item">
             <img src="./images/portfolio_afb/23_what2pack/homescreen.png" alt="Image 1" />
-            <div className="overlay">
-              <div className="tags">
-                <span className="tag">Tag 1</span>
-                <span className="tag">Tag 2</span>
+            <div className="tags">
+                <span className="tag">Web-progamming</span>
+                <span className="tag">UX/UI</span>
+                <span className="tag">Design</span>
               </div>
+            {/* <div className="overlay">
               <motion.div className="text">
                 <p>Some text over image 1</p>
-                <motion.button whileHover={{ scale: 1.1 }}>View More</motion.button>
+                <motion.button>View More</motion.button>
               </motion.div>
-            </div>
+            </div> */}
+          <h2 className="image-title">What2Pack</h2>
+          <p className="image-subtitle">What2Pack extra information</p>
           </motion.div>
 
           {/* Image 2 */}
           <motion.div className="gallery-item">
             <img src="./images/portfolio_afb/22_workbuddy/5.1_final2.png" alt="Image 2" />
-            <div className="overlay">
               <div className="tags">
-                <span className="tag">Tag 1</span>
-                <span className="tag">Tag 2</span>
+                <span className="tag">UX/UI</span>
+                <span className="tag">Design</span>
               </div>
+              {/* <div className="overlay">
               <motion.div className="text">
                 <p>Some text over image 2</p>
-                <motion.button whileHover={{ scale: 1.1 }}>View More</motion.button>
+                <motion.button>View More</motion.button>
               </motion.div>
-            </div>
+            </div> */}
+            <h2 className="image-title">What2Pack</h2>
+            <p className="image-subtitle">What2Pack extra information</p>
           </motion.div>
 
           {/* Image 3 */}
           <motion.div className="gallery-item">
             <img src="./images/portfolio_afb/16_ipmedt1_museum/bipmedt1_rainstorm.png" alt="Image 3" />
-            <div className="overlay">
               <div className="tags">
-                <span className="tag">Tag 1</span>
-                <span className="tag">Tag 2</span>
+                <span className="tag">Hardware</span>
+                <span className="tag">UX/UI</span>
+                <span className="tag">Design</span>
               </div>
+              {/* <div className="overlay">
               <motion.div className="text">
                 <p>Some text over image 3</p>
-                <motion.button whileHover={{ scale: 1.1 }}>View More</motion.button>
+                <motion.button >View More</motion.button>
               </motion.div>
-            </div>
+            </div> */}
+            <h2 className="image-title">What2Pack</h2>
+            <p className="image-subtitle">What2Pack extra information</p>
           </motion.div>
         </div>
-       
       </motion.div>
     </motion.div>
   );
