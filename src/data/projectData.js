40cb0cb9ee1aaabcaf63095a87faa46a1559c8ab
xFleur <mels1999@live.nl>
2025-05-02 00:39:55 +0200

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -5,7 +5,7 @@ const projects = [
     subtitle: "the website fo back for your travel",
     description: "The aim of this website is that you can find information and prepare yourself for your next planned trip. Based on your destination and travel dates.",
     tags: ["Web-programming", "UX/UI", "Design"],
-    images: ["dist/images/portfolio_afb/23_what2pack/startscreen.png", 
+    images: ["../images/portfolio_afb/23_what2pack/startscreen.png", 
       "../images/portfolio_afb/23_what2pack/homescreen.png",
       "../images/portfolio_afb/23_what2pack/5.2_login.png", 
       "../images/portfolio_afb/23_what2pack/5.2_pack.png"]
@@ -17,7 +17,7 @@ const projects = [
     subtitle: "'Using your empathy to help you take care of yourself'",
     description: "Workbuddy is A dedicated, cute, physico-digital object that appeals to your empathy to help you take care of… yourself. A product deliberately designed to limit and prevent you from doing something (over-working). How it works: You plan and set your break schedule or philosophy (you can use preset methods such as the Pomodoro Technique) in the app. Work buddy will detect your work activity and will let you know it's time for a break using low-profile physical interactions (sounds, movement and 'facial expression'). You can then take the break, or 'snooze' Work Buddy, by physically interacting with it (by stroking it, for example). It will (physically) react accordingly. If you snooze it too much, hopefully you get moved by its miserable looking state and just take that break already!",
     tags: ["UX/UI", "Design"],
-    images: ["dist/images/portfolio_afb/22_workbuddy/5.1_prototype.png", 
+    images: ["../images/portfolio_afb/22_workbuddy/5.1_prototype.png", 
       "../images/portfolio_afb/22_workbuddy/5.1_final.png",
       "../images/portfolio_afb/22_workbuddy/5.1_prototype.png", 
       "../images/portfolio_afb/22_workbuddy/Screenshot 2023-05-15 at 21.24.25.png"]
@@ -28,22 +28,22 @@ const projects = [
     subtitle: "Feeding handbook for newborns",
     description: "NurtureTogether is an inclusive app designed to provide (breast)feeding guidance to all newborn caregivers with a focus on parental well-being.",
     tags: ["Web-programming", "UX/UI", "Design"],
-    images: ["dist/images/portfolio_afb/23_nuture/nuture_poster.jpg", 
+    images: ["../images/portfolio_afb/23_nuture/nuture_poster.jpg", 
       "../images/portfolio_afb/23_nuture/nuture_screen.png",
       "../images/portfolio_afb/23_nuture/nuture_wire.png"]
 
-  },
-  {
-    id: "Genetica",
-    title: "Genetica",
-    subtitle: "Learn all about the building blocks of your body, heredity and chromosomes",
-    description: "Our aim is to create a platform that offers users basic information about genetics in an interactive manner. It will be time-consuming to incorporate all genetic information into the platform, so we have opted for basic information. Our goal is to elevate users' basic knowledge and provide them with a better understanding of the world of genetics.",
-    tags: ["Web-programming", "UX/UI", "Design"],
-    images: ["dist/images/portfolio_afb/19_genitica/gen_hifi.png", 
-      "../images/portfolio_afb/19_genitica/gen_tech.png",
-      "../images/portfolio_afb/19_genitica/gen_wire.png", 
-      "../images/portfolio_afb/23_what2pack/5.2_pack.png"]
   }
+  // {
+  //   id: "Genetica",
+  //   title: "Genetica",
+  //   subtitle: "Learn all about the building blocks of your body, heredity and chromosomes",
+  //   description: "Our aim is to create a platform that offers users basic information about genetics in an interactive manner. It will be time-consuming to incorporate all genetic information into the platform, so we have opted for basic information. Our goal is to elevate users' basic knowledge and provide them with a better understanding of the world of genetics.",
+  //   tags: ["Web-programming", "UX/UI", "Design"],
+  //   images: ["../images/portfolio_afb/19_genitica/gen_hifi.png", 
+  //     "../images/portfolio_afb/19_genitica/gen_tech.png",
+  //     "../images/portfolio_afb/19_genitica/gen_wire.png", 
+  //     "../images/portfolio_afb/23_what2pack/5.2_pack.png"]
+  // }
 ];
 
 export default projects;
